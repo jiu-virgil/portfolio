3265f2fbb5d331af6ca074c2718b40013d173c7c
Virgil <jiuvirgil@gmail.com>
2023-03-08 20:46:05 +0200

    Added the projects section and small fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div
-    class="grid items-center w-full min-h-screen pb-5 px-[10%] pt-10 gap-10 grid-cols-[auto_minmax(auto,70ch)] content-end justify-start">
+    class="grid items-center w-full min-h-screen pb-5 px-[10%] pt-10 gap-10 grid-cols-[auto_1fr] content-end justify-start">
     <div class="h-[10rem] relative">
         <div
             class="h-[10rem] w-[10rem] absolute -left-[6rem] bg-white rounded-full" />
@@ -32,7 +32,7 @@
         <h3>2021 - present</h3>
     </div>
     <!--  -->
-    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="self-start w-[4rem] h-[4rem] bg-white rounded-full" />
     <div class="flex flex-col">
         <h3>Worked as a Freelancer on Freelancer.com for 20+ customers</h3>
         <h3>Ian 2020 - Oct 2021</h3>
@@ -68,6 +68,69 @@
 
     <div
         class="right-10 bottom-10 absolute w-[60vh] h-[60vh] bg-white rounded-full" />
+</div>
+
+<div
+    class="grid items-center w-full min-h-screen pb-5 px-[10%] pt-5 gap-10 grid-cols-[auto_minmax(auto,1fr)] content-between justify-start">
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>Projects</h3>
+        <div class="flex items-center gap-2 text-[#B4B4B4] font-medium">
+            <div class="w-[1.5rem] h-[1.5rem] bg-white rounded-full" />
+            <p>jiu-virgil</p>
+        </div>
+    </div>
+
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+
+    <div class="flex w-full justify-between gap-20">
+        <div class="flex justify-center flex-col gap-2">
+            <h3>Scheduler for various small businesses</h3>
+            <p class="text-[#B4B4B4] font-medium">
+                Built for a tattoo salon so that customers could see how booked
+                the artists are before calling for a session.
+            </p>
+            <p class="text-[#B4B4B4] font-medium">
+                Since then I rewrote it to be more modular, so that it could be
+                used in other small businesses with similar needs, such as nail
+                salons, dentists, dog walkers, etc.
+            </p>
+            <div class="pt-2 flex gap-5">
+                {#each Array(5) as _}
+                    <div class="w-[2rem] h-[2rem] bg-white rounded-full" />
+                {/each}
+            </div>
+        </div>
+
+        <div class="w-[600px] h-[350px] bg-white rounded-xl" />
+    </div>
+
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex w-full justify-between gap-20">
+        <div class="justify-center flex flex-col gap-2">
+            <h3>Landing page and portfolio template</h3>
+            <p class="text-[#B4B4B4] font-medium">
+                Built for a solar installation company.
+            </p>
+            <p class="text-[#B4B4B4] font-medium">
+                Simple, static, 3 page website, with an almost perfect
+                lighthouse score thanks to using one of the latest emerging
+                stacks.
+            </p>
+            <div class="pt-2 flex gap-5">
+                {#each Array(5) as _}
+                    <div class="w-[2rem] h-[2rem] bg-white rounded-full" />
+                {/each}
+            </div>
+        </div>
+
+        <div class="w-[600px] h-[350px] bg-white rounded-xl" />
+    </div>
+
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>Let's talk</h3>
+    </div>
 </div>
 
 <style>
