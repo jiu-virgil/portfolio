355f43b75e735e2c0007a0f13311a1d831d5b5bc
Virgil <jiuvirgil@gmail.com>
2023-03-08 17:41:24 +0200

    Main page block up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,68 @@
-<h1>Portfolio</h1>
+<div
+    class="grid items-center w-full h-screen px-32 pb-10 gap-10 grid-cols-[auto_50%] content-end justify-start">
+    <div class="h-[10rem] relative">
+        <div
+            class="h-[10rem] w-[10rem] absolute -left-[6rem] bg-white rounded-full" />
+    </div>
+    <div class="flex flex-col text-[1.5rem]">
+        <h1 class="leading-[5rem] pb-5 font-bold">
+            Virgil Constantin's<br />Portfolio
+        </h1>
+    </div>
+
+    <div />
+    <h2>Web Developer & Designer</h2>
+
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>Studying at POLITEHNICA University of Bucharest</h3>
+        <h3>2021 - present</h3>
+    </div>
+    <!--  -->
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>Worked as a Freelancer on Freelancer.com for 20+ customers</h3>
+        <h3>Ian 2020 - Oct 2021</h3>
+        <p class="text-[#B4B4B4] font-medium">
+            Various projects for individuals and small businesses where I
+            developed multithreaded scripts, GUIs for APIs, database models,
+            optimized & reviewed code and built fast, static websites.
+        </p>
+    </div>
+    <!--  -->
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>My skills:</h3>
+        <p class="text-[#B4B4B4] font-medium">
+            Svelte, React, Flask, MySQL, MongoDB, Prisma, Javascript, Scss
+            Selenium, BeautifulSoup, Python, C#, basic C++ Figma, Illustrator,
+            GitHub.
+        </p>
+    </div>
+    <!--  -->
+    <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    <div class="flex flex-col">
+        <h3>My resume</h3>
+    </div>
+    <div>
+        <div class="w-[4rem] h-[4rem] bg-white rounded-full" />
+    </div>
+    <!--  -->
+    <div class="flex flex-col">
+        <h3>Projects</h3>
+    </div>
+    <div
+        class="right-10 bottom-10 absolute w-[40vw] h-[40vw] bg-white rounded-full" />
+</div>
+
+<style>
+    h1 {
+        font-size: 4rem;
+    }
+    h2 {
+        font-size: 1.8rem;
+    }
+    h3 {
+        font-size: 1.5rem;
+    }
+</style>
